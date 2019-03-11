bc3794a3edf64fe2e2f5c76f1f2baa5e8931b088
MHIRI Mohamed <mmhiri@amaris.com>
2019-03-11 17:24:15 +0100

    ilogger added to log4net

--- Program.cs
+++ Program.cs
@@ -12,13 +12,14 @@ namespace LoggingExample
 {
     public class Program
     {
-        private static readonly log4net.ILog log = log4net.LogManager
-            .GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        /* private static readonly log4net.ILog log = log4net.LogManager
+            .GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);*/
         public static void Main(string[] args)
         {
-             var logRepository = log4net.LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
+             //var logRepository = log4net.LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
 
             //Load configuration from log4net.config file
+            /*
             log4net
                 .Config
                 .XmlConfigurator
@@ -26,9 +27,10 @@ namespace LoggingExample
                     logRepository,
                     new System.IO.FileInfo("log4net.config")
                 );
+            */
             Console.WriteLine("Hello World!");
 
-            log.Info("Log4net for .Net Core Hello World");
+            //log.Info("Log4net for .Net Core Hello World");
             
             CreateWebHostBuilder(args).Build().Run();
 
